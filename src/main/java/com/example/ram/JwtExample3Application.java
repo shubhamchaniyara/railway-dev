52d9b4fc3c2f7970e0da6efd566c87247e44d0a5
Shubham Chaniyara <126853709+shubhamchaniyara@users.noreply.github.com>
2024-06-14 20:58:08 +0000

    fdx

--- src/main/java/com/example/ram/JwtExample3Application.java
+++ src/main/java/com/example/ram/JwtExample3Application.java
@@ -1,4 +1,4 @@
-package com.jwt.example;
+package com.example.ram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/ram/controller/HomeController.java
+++ src/main/java/com/example/ram/controller/HomeController.java
@@ -1,3 +1,5 @@
+package com.example.ram.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
